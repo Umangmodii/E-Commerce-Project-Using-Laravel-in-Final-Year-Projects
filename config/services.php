bf47025d3cf1716b4a7fe9401930470c3744c4a5
umangmodii <Umangmodi003@gmail.com>
2024-03-30 00:04:53 +0530

    commit

--- config/services.php
+++ config/services.php
@@ -29,21 +29,21 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-    'github' => [
-        'client_id' => '8d56f3806bcad45fc301', //Github API
-        'client_secret' => 'a074b62bcef338439ccec78540ffd76bb405416f', //Github Secret
-        'redirect' => 'http://localhost:8000/login/github/callback',
-     ],
-     'google' => [
-        'client_id' => '792082097032-pb2bjroi1mbs5iu7p0hkk4835cur69i1.apps.googleusercontent.com', //Google API
-        'client_secret' => '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', //Google Secret
-        'redirect' => 'http://localhost:8000/login/google/callback',
-     ],
-     'facebook' => [
-        'client_id' => '6865812653504450', //Facebook API
-        'client_secret' => '3a5fe04a46a0cf82efc741c546ddda20', //Facebook Secret
-        'redirect' => 'http://localhost:8000/login/facebook/callback',
-     ],
+    // 'github' => [
+    //     'client_id' => '8d56f3806bcad45fc301', //Github API
+    //     'client_secret' => 'a074b62bcef338439ccec78540ffd76bb405416f', //Github Secret
+    //     'redirect' => 'http://localhost:8000/login/github/callback',
+    //  ],
+    //  'google' => [
+    //     'client_id' => '792082097032-pb2bjroi1mbs5iu7p0hkk4835cur69i1.apps.googleusercontent.com', //Google API
+    //     'client_secret' => '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', //Google Secret
+    //     'redirect' => 'http://localhost:8000/login/google/callback',
+    //  ],
+    //  'facebook' => [
+    //     'client_id' => '6865812653504450', //Facebook API
+    //     'client_secret' => '3a5fe04a46a0cf82efc741c546ddda20', //Facebook Secret
+    //     'redirect' => 'http://localhost:8000/login/facebook/callback',
+    //  ],
 
 
 ];
